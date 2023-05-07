a3907109c21d82de5b0f04916d6fa2eb82e54985
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-07 13:18:08 -0300

    export all and export IBrandReposity

--- src/app/ports/repositories/brand/index.ts
+++ src/app/ports/repositories/brand/index.ts
@@ -1,3 +1,16 @@
-import { IDeleteBrandRepository } from "./delete";
+import { IDeleteBrandRepository } from "./delete"
+import { ISaveBrandRepository } from "./save"
+import { IUpdateBandRepository } from "./update"
+import { IFindAllBrandRepository } from "./find-all"
+import { IFindByIdBrandRepository } from "./find-by-id"
+import { IFindSearchBrandRepository, IFindSearchBrandRepositoryDTO } from "./find-search"
 
-export type IBrandRepository = IDeleteBrandRepository
+type IBrandRepository = IDeleteBrandRepository & ISaveBrandRepository &
+  IUpdateBandRepository & IFindAllBrandRepository & IFindByIdBrandRepository &
+  IFindSearchBrandRepository
+export {
+  IBrandRepository,
+  IDeleteBrandRepository, ISaveBrandRepository, IUpdateBandRepository,
+  IFindAllBrandRepository, IFindByIdBrandRepository, IFindSearchBrandRepository,
+  IFindSearchBrandRepositoryDTO
+}
