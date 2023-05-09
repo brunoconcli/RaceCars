0fbe34fa3946302e34bac40f233daf3363201bc8
Bruno Concli <brunoconcli@gmail.com>
2023-05-09 16:42:38 -0300

    reaf: add key including option Co-authored-by: Miguel Lopes Braido <miguel-l-b@users.noreply.github.com>

--- src/infrastructure/repositories/mysql/join-modular-values.ts
+++ src/infrastructure/repositories/mysql/join-modular-values.ts
@@ -1,7 +1,15 @@
-export default function joinModularValues(data: object): string {
+export default function joinModularValues(
+  data: object,
+  includeKey = true
+): string {
   const values: string[] = []
   for (const key in data)
-    if (data[key]) values.push(`${key} = ${data[key] as string}`)
+    if (data[key])
+      values.push(
+        `${includeKey ? `${key} = ` : ""}${
+          typeof data[key] !== "number" ? `'${data[key] as string}'` : ""
+        }`
+      )
 
   return values.join(", ")
 }
