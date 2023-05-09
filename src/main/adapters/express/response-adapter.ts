655ad72bd3bcdc0b2f988700254782231f31a921
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-08 23:37:47 -0300

    feat: adapt of response

---
+++ src/main/adapters/express/response-adapter.ts
@@ -0,0 +1,14 @@
+import { type Response as httpResponse } from "express"
+
+import { type IResponse } from "@app/ports/presentation"
+
+export function adaptResponseExpress(
+  response: IResponse
+): (res: httpResponse) => void {
+  return (res: httpResponse) => {
+    response.statusCode && res.status(response.statusCode)
+    typeof response.body === "object" && res.json(response.body)
+    typeof response.body === "string" && res.send(response.body)
+    return response
+  }
+}
