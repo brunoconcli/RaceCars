efa67589ed773379909522857c5a4a6c6cf977b4
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-08 23:36:28 -0300

    refactor: making it possible to return an IRequest

--- src/app/ports/presentation/middleware.ts
+++ src/app/ports/presentation/middleware.ts
@@ -1,5 +1,5 @@
-import { IRequest, IResponse } from '.'
+import { type IRequest, type IResponse } from "."
 
 export interface IMiddleware {
-  handle: (request: IRequest) => Promise<IResponse>
+  handle: (request: IRequest, next: () => void) => IResponse | any
 }
