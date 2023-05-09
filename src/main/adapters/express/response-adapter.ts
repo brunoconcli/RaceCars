d920aeca0ee8d3c8a315e920d25db94e6be730da
Bruno Concli <brunoconcli@gmail.com>
2023-05-09 16:46:17 -0300

    reaf: verifies if body is null Co-authored-by: Miguel Lopes Braido <miguel-l-b@users.noreply.github.com>

--- src/main/adapters/express/response-adapter.ts
+++ src/main/adapters/express/response-adapter.ts
@@ -6,7 +6,9 @@ export function adaptResponseExpress(
   response: IResponse
 ): (res: httpResponse) => void {
   return (res: httpResponse) => {
-    response.statusCode && res.status(response.statusCode)
+    response.body
+      ? res.status(response.statusCode)
+      : res.sendStatus(response.statusCode)
     typeof response.body === "object" && res.json(response.body)
     typeof response.body === "string" && res.send(response.body)
     return response
