f45776999010df2e75162f933212995a7c5fff27
Bruno Concli <brunoconcli@gmail.com>
2023-05-10 14:52:22 -0300

    reaf: Adapting to prettier format

--- src/app/use-cases/brand/CreateBrand.ts
+++ src/app/use-cases/brand/CreateBrand.ts
@@ -1,18 +1,24 @@
-import { Brand } from "@core/entities"
-import { ICreateBrand, ICreateBrandDTO } from "@core/use-cases"
-import { IFindSearchBrandRepository, ISaveBrandRepository } from "@app/ports/repositories"
+import { type Brand } from "@core/entities"
+import { type ICreateBrand, type ICreateBrandDTO } from "@core/use-cases"
+
+import {
+  type IFindSearchBrandRepository,
+  type ISaveBrandRepository,
+} from "@app/ports/repositories"
 
 export class CreateBrand implements ICreateBrand {
-  constructor (
+  constructor(
     private readonly findSearchBrandRepository: IFindSearchBrandRepository,
     private readonly saveBrandRepository: ISaveBrandRepository
   ) {}
 
-  async create (data: ICreateBrandDTO) : Promise<Brand> {
-    if (await this.findSearchBrandRepository.findSearch({
-      name: data.name,
-      country: data.country
-    }))
+  async create(data: ICreateBrandDTO): Promise<Brand> {
+    if (
+      await this.findSearchBrandRepository.findSearch({
+        name: data.name,
+        country: data.country,
+      })
+    )
       throw new Error("This brand already exists")
 
     return await this.saveBrandRepository.save(data)
