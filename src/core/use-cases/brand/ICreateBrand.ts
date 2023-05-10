d1f10715213657389d5ba60c6b87d53ed4fc48b1
Bruno <brunoconcli@gmail.com>
2023-05-09 23:07:52 -0300

    fix: Change DTO name from type to interface

--- src/core/use-cases/brand/ICreateBrand.ts
+++ src/core/use-cases/brand/ICreateBrand.ts
@@ -1,11 +1,11 @@
 import { type Brand } from "@core/entities"
 
-export interface TCreateBrandDTO {
+export interface ICreateBrandDTO {
   readonly name: Brand["name"]
   readonly country: Brand["country"]
   readonly inauguratedIn: Brand["inauguratedIn"]
 }
 
 export interface ICreateBrand {
-  create: (brand: TCreateBrandDTO) => Promise<Brand>
+  create: (brand: ICreateBrandDTO) => Promise<Brand>
 }
