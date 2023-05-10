fe681f9351de406b4e93a930de080b36dedad179
Bruno <brunoconcli@gmail.com>
2023-05-09 23:10:20 -0300

    fix: Save returns RaceCar not void

--- src/app/ports/repositories/race-car/save.ts
+++ src/app/ports/repositories/race-car/save.ts
@@ -1,5 +1,5 @@
 import { type RaceCar } from "@core/entities"
 
 export interface ISaveRaceCarRepository {
-  save: (data: Omit<RaceCar, "id">) => Promise<void>
+  save: (data: Omit<RaceCar, "id">) => Promise<RaceCar>
 }
