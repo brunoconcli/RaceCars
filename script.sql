7a026db5f9e5e43409c0272b6b687a5b28848eb2
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-10 21:38:38 -0300

    update script

--- script.sql
+++ script.sql
@@ -1,20 +1,27 @@
 USE cotuca;
 
-CREATE TABLE Brand (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  country char(2) NOT NULL
-);
+SELECT * FROM Brand;
+-- INSERT INTO Brand (name, country, inauguratedIn) VALUES ( 'ADOLETA', 'BR', 2022);
 
-CREATE TABLE RaceCar (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  brandId INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  year INT NOT NULL,
-  color CHAR(6) NOT NULL,
-  price INT NOT NULL
-);
+-- DROP TABLE RaceCar;
+-- DROP TABLE Brand;
 
-ALTER TABLE RaceCar
-  ADD CONSTRAINT fk_brand FOREIGN KEY (brandId)
-    REFERENCES Brand(id);
+-- CREATE TABLE Brand (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL,
+--   country char(2) NOT NULL,
+--   inauguratedIn INT NOT NULL
+-- );
+
+-- CREATE TABLE RaceCar (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   brandId INT NOT NULL,
+--   name VARCHAR(30) NOT NULL,
+--   year INT NOT NULL,
+--   color CHAR(6) NOT NULL,
+--   price INT NOT NULL
+-- );
+
+-- ALTER TABLE RaceCar
+--   ADD CONSTRAINT fk_brand FOREIGN KEY (brandId)
+--     REFERENCES Brand(id);
