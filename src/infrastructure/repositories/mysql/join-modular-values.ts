415710eb74307c3f68320dbc202eae4dfc4343f2
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-10 21:39:39 -0300

    reaf: reformulate function

--- src/infrastructure/repositories/mysql/join-modular-values.ts
+++ src/infrastructure/repositories/mysql/join-modular-values.ts
@@ -1,15 +1,18 @@
 export default function joinModularValues(
   data: object,
-  includeKey = true
+  includeKey = true,
+  separator = ", "
 ): string {
   const values: string[] = []
   for (const key in data)
     if (data[key])
       values.push(
         `${includeKey ? `${key} = ` : ""}${
-          typeof data[key] !== "number" ? `'${data[key] as string}'` : ""
+          typeof data[key] !== "number"
+            ? `'${data[key] as string}'`
+            : `${data[key] as number}`
         }`
       )
 
-  return values.join(", ")
+  return values.join(separator)
 }
