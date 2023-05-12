483134b478d20fc17407d5d2e1a4a5ab2957d847
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-11 23:23:09 -0300

    to-continue: algorithm against SQL injection

--- src/main/factories/middlewares/protect-sql-injection.ts
+++ src/main/factories/middlewares/protect-sql-injection.ts
@@ -1,15 +1,7 @@
-import mysql from "mysql2"
-
 import { type IRequest, type IMiddleware } from "@app/ports/presentation"
 
 export class ProtectSQLInjectionMiddleware implements IMiddleware {
   handle(req: IRequest, next: () => void): any {
-    const { query, body, params } = req
-
-    for (const key in params) params[key] = mysql.escape(params[key])
-    for (const key in body) body[key] = mysql.escape(body[key])
-    for (const key in query) query[key] = mysql.escape(query[key])
-
     next()
   }
 }
