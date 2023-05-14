1a1765ba69f9a387aa7791134acba32edd508830
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-14 19:47:10 -0300

    feat: add new rotes "/create", "/",  "/:id", "/search"

--- src/main/routes/brand.ts
+++ src/main/routes/brand.ts
@@ -1,27 +1,43 @@
 import { BrandRepository } from "@infra/repositories"
 import { adaptControllerExpress } from "@main/adapters/express/controller-adapter"
-import { CreateBrandController } from "@pre/controllers"
+import {
+  CreateBrandController,
+  GetByIdController,
+  GetAllController,
+  GetSearchController,
+} from "@pre/controllers"
 import {
   CompositeValidation,
   RequiredAndTypeFieldValidation,
 } from "@pre/validation"
 import { Router } from "express"
 
-import { CreateBrand } from "@app/use-cases/brand"
+import { CreateBrand, GetBrand } from "@app/use-cases/brand"
 
 const routes = Router()
-
+const brandRepository = new BrandRepository()
 routes.post(
   "/create",
   adaptControllerExpress(
     new CreateBrandController(
-      new CreateBrand(new BrandRepository(), new BrandRepository()),
+      new CreateBrand(brandRepository, brandRepository),
       new CompositeValidation([
         new RequiredAndTypeFieldValidation("name", "string"),
         new RequiredAndTypeFieldValidation("country", "string"),
         new RequiredAndTypeFieldValidation("inauguratedIn", "number"),
       ])
     )
+  )
+)
+
+// GETERS
+const getBrand = new GetBrand(brandRepository, brandRepository, brandRepository)
+routes.get("/", adaptControllerExpress(new GetAllController(getBrand)))
+routes.get("/search", adaptControllerExpress(new GetSearchController(getBrand)))
+routes.get(
+  "/:id",
+  adaptControllerExpress(
+    new GetByIdController(getBrand, new RequiredAndTypeFieldValidation("id"))
   )
 )
 
