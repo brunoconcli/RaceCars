29279b0c98a70da20c0bb9744d9b239c42b1ad6c
brunoconcli <brunoconcli@gmail.com>
2023-05-15 16:56:28 -0300

    feat: implemention find all and find by id

--- src/infrastructure/repositories/mysql/race-car.ts
+++ src/infrastructure/repositories/mysql/race-car.ts
@@ -1,22 +1,30 @@
-// import { type RaceCar } from "@core/entities"
-// import { getConnection } from "@infra/providers/mysql-connection"
+import { type RaceCar } from "@core/entities"
+import { getConnection } from "@infra/providers/mysql-connection"
 
-// import {
-//   type IRaceCarRepository,
-//   type IFindSearchRaceCarRepositoryDTO,
-// } from "@app/ports/repositories"
+import {
+  type IRaceCarRepository,
+  type IFindSearchRaceCarRepositoryDTO,
+} from "@app/ports/repositories"
 
-// import joinModularValues from "./join-modular-values"
+import joinModularValues from "./join-modular-values"
 
-// export class RaceCar implements IRaceCarRepository {
-//   async findAll(): Promise<RaceCar[]> {
-//     const connection = await getConnection()
-//     return await connection
-//       .execute("SELECT * FROM RaceCar")
-//       .then((results) =>results[0] as RaceCar[])
-//       .catch(() => {
-//         throw new Error("Error in findind all Race cars")
-//       })
-//   }
-//   async findById(data:)
-// }
+export class RaceCar implements IRaceCarRepository {
+  async findAll(): Promise<RaceCar[]> {
+    const connection = await getConnection()
+    return await connection
+      .execute("SELECT * FROM RaceCar")
+      .then((results) =>results[0] as RaceCar[])
+      .catch(() => {
+        throw new Error("Error in findind all Race cars")
+      })
+  }
+  async findById(id: number) {
+    const connection = await getConnection()
+    return await connection
+    .query("SELECT * FROM RaceCar WHERE id = ?", [id])
+    .then((results) => results[0][0] as RaceCar)
+    .catch(() => {
+        
+    })
+  }
+}
