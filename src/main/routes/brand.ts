fc29f41561ece71a4a7b13dc7f1740cf6f4b5498
miguel-l-b <miguellopesbraido@gmail.com>
2023-05-16 00:14:33 -0300

    feat: implementate route update

--- src/main/routes/brand.ts
+++ src/main/routes/brand.ts
@@ -7,15 +7,20 @@ import {
   GetSearchController,
 } from "@pre/controllers"
 import { DeleteBrandController } from "@pre/controllers/brand/delete"
+import { UpdateController } from "@pre/controllers/brand/update"
 import { ParseToTypeRule, SetDefaultValueRule } from "@pre/rule"
 import { CompositeRule } from "@pre/rule/composite"
 import {
   CompositeValidation,
+  TypeFieldVerification,
+  DefaultValueValidation,
   RequiredAndTypeFieldValidation,
 } from "@pre/validation"
 import { Router } from "express"
 
 import { CreateBrand, DeleteBrand, GetBrand } from "@app/use-cases/brand"
+import { UpdateBrand } from "@app/use-cases/brand/UpdateBrand"
+
 
 const routes = Router()
 const brandRepository = new BrandRepository()
@@ -32,6 +37,21 @@ routes.post(
     )
   )
 )
+routes.put(
+  "/update/:id",
+  adaptControllerExpress(
+    new UpdateController(
+      new UpdateBrand(brandRepository, brandRepository),
+      new CompositeValidation([
+        new TypeFieldVerification("name", "string"),
+        new TypeFieldVerification("country", "string"),
+        new TypeFieldVerification("inauguratedIn", "number"),
+      ]),
+      new ParseToTypeRule<number>("id"),
+      new RequiredAndTypeFieldValidation("id", "number")
+    )
+  )
+)
 routes.delete(
   "/delete/:id",
   adaptControllerExpress(
@@ -42,7 +62,6 @@ routes.delete(
   )
 )
 
-// GETERS
 const getBrand = new GetBrand(brandRepository, brandRepository, brandRepository)
 routes.get("/", adaptControllerExpress(new GetAllController(getBrand)))
 routes.get(
@@ -52,10 +71,20 @@ routes.get(
       getBrand,
       new CompositeRule([
         new SetDefaultValueRule("filter", {
-          offset: 0,
+          page: 1,
           limit: 10,
           order: "ASC",
         }),
+      ]),
+      new CompositeValidation([
+        new RequiredAndTypeFieldValidation("filter", "object"),
+        new RequiredAndTypeFieldValidation("filter.page", "number"),
+        new RequiredAndTypeFieldValidation("filter.limit", "number"),
+        new RequiredAndTypeFieldValidation("filter.order", "string"),
+        new DefaultValueValidation("filter.order", ["ASC", "DESC"]),
+        new TypeFieldVerification("name", "string"),
+        new TypeFieldVerification("inauguratedIn", "number"),
+        new TypeFieldVerification("country", "string"),
       ])
     )
   )
