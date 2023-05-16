d938af89dd4b6c34d5014584d1cbdc56b3aac7a7
Bruno <brunoconcli@gmail.com>
2023-05-15 21:49:36 -0300

    feat: Implemented mysql for race-car in infra

--- src/infrastructure/repositories/mysql/race-car.ts
+++ src/infrastructure/repositories/mysql/race-car.ts
@@ -8,7 +8,52 @@ import {
 
 import joinModularValues from "./join-modular-values"
 
-export class RaceCar implements IRaceCarRepository {
+export class RaceCarRepository implements IRaceCarRepository {
+  async delete(id: number): Promise<void> {
+    const connection = await getConnection()
+    await connection
+      .execute("DELETE * FROM RaceCar WHERE id = ?", [id])
+      .then(() => {})
+      .catch(() => {
+        throw new Error("Error on delete racecar")
+      })
+  }
+
+  async save(data: Omit<RaceCar, "id">): Promise<RaceCar> {
+    if (
+      (await this.findSearch({
+        name: data.name,
+        brand: data.brandId,
+        color: data.color,
+        year: [data.year],
+        priceMin: data.price,
+      })).length > 0
+    )
+    throw new Error("Racecar already exists")
+    const connection = await getConnection()
+    await connection.execute(
+      "INSERT INTO RaceCar (name, brand, color, year, priceMin) VALUES (?, ?, ?, ?, ?, ?)",
+      [data.name, data.brandId, data.color, data.year, data.price]
+    )
+    return await this.findSearch({
+      name: data.name,
+      brand: data.brandId,
+      color: data.color,
+      year: [data.year],
+      priceMin: data.price,
+    })[0]
+  }
+
+  async update(id: number, data: Partial<Omit<RaceCar, "id">>): Promise<RaceCar> {
+    const connection = await getConnection()
+    const query =
+      "UPDATE RaceCar SET " + joinModularValues(data).join(", ") + "WHERE id = ?"
+
+    await connection.execute(query, [id])
+
+    return await this.findById(id)
+  }
+
   async findAll(): Promise<RaceCar[]> {
     const connection = await getConnection()
     return await connection
@@ -18,13 +63,38 @@ export class RaceCar implements IRaceCarRepository {
         throw new Error("Error in findind all Race cars")
       })
   }
+
   async findById(id: number) {
     const connection = await getConnection()
     return await connection
     .query("SELECT * FROM RaceCar WHERE id = ?", [id])
     .then((results) => results[0][0] as RaceCar)
     .catch(() => {
-        
+        throw new Error("Error on finding racecar")
     })
   }
+
+  async findSearch(data: IFindSearchRaceCarRepositoryDTO): Promise<RaceCar[]> {
+    const connection = await getConnection()
+    let query =
+      "SELECT * FROM RaceCar" +
+      (!data.name && !data.brand && !data.color && !data.year && !data.priceMax && !data.priceMin ? "" : " WHERE ")
+      const add = [
+        ...joinModularValues(
+          { name: data.name, brandId: data.brand, color: data.color, year: data.year, price: data.priceMax },
+          true,
+          " LIKE "
+        ),
+      ]
+      query += add.join(" AND ")
+
+      return await connection
+        .query(query)
+        .then((results) => results[0] as RaceCar[])
+        .catch(() => {
+          throw new Error ("Error on find search racecar")
+        })
+
+  }
+
 }
