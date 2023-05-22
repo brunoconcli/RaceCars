358255a7977a665af257d53607f24b57723c21b7
Bruno <brunoconcli@gmail.com>
2023-05-22 05:33:44 -0300

    feat: Implemented race-car Routes

--- src/main/routes/raceCar.ts
+++ src/main/routes/raceCar.ts
@@ -1,5 +1,119 @@
+import { RaceCarRepository } from "@infra/repositories/mysql/race-car"
+import { adaptControllerExpress } from "@main/adapters/express/controller-adapter"
+import {
+  CreateRaceCarController,
+  DeleteRaceCarController,
+  GetAllRaceCarsController,
+  GetRaceCarByIdController,
+  GetRaceCarsSearchController,
+  UpdateRaceCarController,
+} from "@pre/controllers"
+import { NumberConvert } from "@pre/convert"
+import { ParseToTypeRule, SetDefaultValueRule } from "@pre/rule"
+import { CompositeRule } from "@pre/rule/composite"
+import {
+  CompositeValidation,
+  TypeFieldVerification,
+  RequiredAndTypeFieldValidation,
+  DefaultValueValidation,
+} from "@pre/validation"
 import { Router } from "express"
 
+import {
+  CreateRaceCar,
+  DeleteRaceCar,
+  GetRaceCar,
+  UpdateRaceCar,
+} from "@app/use-cases"
+
 const routes = Router()
+const raceCarRepository = new RaceCarRepository()
+routes.post(
+  "/",
+  adaptControllerExpress(
+    new CreateRaceCarController(
+      new CreateRaceCar(raceCarRepository, raceCarRepository),
+      new CompositeValidation([
+        new RequiredAndTypeFieldValidation("name", "string"),
+        new RequiredAndTypeFieldValidation("brand", "number"),
+        new RequiredAndTypeFieldValidation("color", "string"),
+        new RequiredAndTypeFieldValidation("year", "number"),
+        new RequiredAndTypeFieldValidation("price", "number"),
+      ])
+    )
+  )
+)
+routes.put(
+  "/:id",
+  adaptControllerExpress(
+    new UpdateRaceCarController(
+      new UpdateRaceCar(raceCarRepository, raceCarRepository),
+      new CompositeValidation([
+        new TypeFieldVerification("name", "string"),
+        new TypeFieldVerification("brand", "number"),
+        new TypeFieldVerification("color", "string"),
+        new TypeFieldVerification("year", "number"),
+        new TypeFieldVerification("price", "number"),
+      ]),
+      new RequiredAndTypeFieldValidation("id", "number"),
+      new ParseToTypeRule("id", new NumberConvert())
+    )
+  )
+)
+routes.delete(
+  "/:id",
+  adaptControllerExpress(
+    new DeleteRaceCarController(
+      new DeleteRaceCar(raceCarRepository, raceCarRepository),
+      new ParseToTypeRule("id", new NumberConvert())
+    )
+  )
+)
+const getRaceCar = new GetRaceCar(
+  raceCarRepository,
+  raceCarRepository,
+  raceCarRepository
+)
+routes.get(
+  "/",
+  adaptControllerExpress(new GetAllRaceCarsController(getRaceCar))
+)
+routes.get(
+  "/search",
+  adaptControllerExpress(
+    new GetRaceCarsSearchController(
+      getRaceCar,
+      new CompositeRule([
+        new SetDefaultValueRule("filter", {
+          page: 1,
+          limit: 10,
+          order: "ASC",
+        }),
+      ]),
+      new CompositeValidation([
+        new RequiredAndTypeFieldValidation("filter", "object"),
+        new RequiredAndTypeFieldValidation("filter.page", "number"),
+        new RequiredAndTypeFieldValidation("filter.limit", "number"),
+        new RequiredAndTypeFieldValidation("filter.order", "string"),
+        new DefaultValueValidation("filter.order", ["ASC", "DESC"]),
+        new TypeFieldVerification("name", "string"),
+        new TypeFieldVerification("brand", "number"),
+        new TypeFieldVerification("color", "string"),
+        new TypeFieldVerification("year", "number"),
+        new TypeFieldVerification("priceMax", "number"),
+        new TypeFieldVerification("priceMin", "number"),
+      ])
+    )
+  )
+)
+routes.get(
+  "/:id",
+  adaptControllerExpress(
+    new GetRaceCarByIdController(
+      new GetRaceCar(raceCarRepository, raceCarRepository, raceCarRepository),
+      new RequiredAndTypeFieldValidation("id")
+    )
+  )
+)
 
 export default routes
